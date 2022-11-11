6312f1b3358e64544069c131b84223183d0384ff
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-11-11 14:24:33 -0500

    Add: emit messages to client

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  })
+  
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
